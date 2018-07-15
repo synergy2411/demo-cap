5e038ba89e1287dc162c159423ed1de1d38490b8
synergy2411 <synergy2411@outlook.com>
2018-07-15 10:44:39 +0530

    custom attribute directive added

---
+++ src/app/directives/highlight.directive.ts
@@ -0,0 +1,25 @@
+import { Directive, ElementRef, HostListener, HostBinding, Input } from '@angular/core';
+
+@Directive({
+    selector : '[apphighlight]'
+})
+export class HighlightDirective{
+
+    @Input('myColor') myColor : string;
+    @HostBinding('style.backgroundColor') bgColor : string;
+
+    @HostListener('mouseleave') 
+    mouseleave(eventdata){
+        //alert('Mouse Entered!');
+        this.bgColor = "transparent";
+    }
+    @HostListener('mouseenter') 
+    mouseenter(eventdata){
+        //alert('Mouse Entered!');
+        this.bgColor = this.myColor;
+    }
+    constructor(private elementRef : ElementRef){
+        //console.log(this.elementRef.nativeElement);
+        //this.elementRef.nativeElement.style.backgroundColor = "aqua";
+    }
+}
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -24,25 +24,33 @@ export class UserComponent implements OnInit{
     @Input('users') users : User[];
     @Output('childEvent') childEvent = new EventEmitter<string>();
 
-    constructor(){
-        console.log('constructor');
+    myClasses = {
+        myBorder : true,
+        myText : false
     }
-    ngOnInit(){
-        console.log("ngOnInit");
-    }
-    ngOnChanges(changes : SimpleChanges){console.log("ngOnChanges", changes);}
-    ngDoCheck(){console.log("ngDoCheck");}
-    ngAfterContentInit(){console.log("ngAfterContentInit");}
-    ngAfterContentChecked(){console.log("ngAfterContentChecked");}
-    ngAfterViewInit(){console.log("ngAfterViewInit");}
-    ngAfterViewChecked(){console.log("ngAfterViewChecked");}
-    ngOnDestroy(){console.log("ngOnDestroy");}
+
+    myColor = "green";
+    // constructor(){
+    //     console.log('constructor');
+    // }
+    // ngOnInit(){
+    //     console.log("ngOnInit");
+    // }
+    // ngOnChanges(changes : SimpleChanges){console.log("ngOnChanges", changes);}
+    // ngDoCheck(){console.log("ngDoCheck");}
+    // ngAfterContentInit(){console.log("ngAfterContentInit");}
+    // ngAfterContentChecked(){console.log("ngAfterContentChecked");}
+    // ngAfterViewInit(){console.log("ngAfterViewInit");}
+    // ngAfterViewChecked(){console.log("ngAfterViewChecked");}
+    // ngOnDestroy(){console.log("ngOnDestroy");}
 
     onKeyup(value : string){
         this.childEvent.emit(value);
     }
 
     moreInfo(user : User){
+        this.myClasses.myBorder = false;
+        this.myClasses.myText = true;
         alert(`${user.firstName } is working with ${user.company}!!`);
     }
     
